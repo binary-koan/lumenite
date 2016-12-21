4b1be9af8238575e06ccba1ad857245f13bbece7
Jono Mingard <reason.koan@gmail.com>
2016-12-21 21:54:22 +1300

    Test displaying field data in tabs

--- app/src/components/tabs/index.vue
+++ app/src/components/tabs/index.vue
@@ -10,16 +10,24 @@
 <template>
   <div class="tabs">
     <tab-bar></tab-bar>
+    <fields :schema="settingsSchema" :layout="settingsLayout"></fields>
   </div>
 </template>
 
 <script>
   import TabBar from './tab-bar'
+  import Fields from './settings/fields'
+
+  import { default as settingsSchema, layout as settingsLayout } from 'src/filesystem/schemas/settings/general'
 
   export default {
     name: 'tabs',
+    data() {
+      return { settingsSchema, settingsLayout }
+    },
     components: {
-      TabBar
+      TabBar,
+      Fields
     }
   }
 </script>
---
+++ app/src/components/tabs/settings/fields.vue
@@ -0,0 +1,22 @@
+<style lang="stylus" scoped>
+</style>
+
+<template>
+  <div class="fields">
+    <div class="group" v-for="group in layout.groups">
+      <h3 v-if="group.title">{{ group.title }}</h3>
+      <div v-for="row in group.fields">
+        <span v-for="field in row">
+          {{ schema[field] }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'fields',
+    props: ['schema', 'layout']
+  }
+</script>
