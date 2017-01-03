945324b882981cf9f37499a23af04732a2a8a51f
Jono Mingard <reason.koan@gmail.com>
2017-01-03 14:25:07 +1300

    Disallow multiple tabs of the same file

--- app/src/store/tabs/actions/open-file.js
+++ app/src/store/tabs/actions/open-file.js
@@ -1,4 +1,5 @@
 import find from 'lodash/find'
+import findIndex from 'lodash/findIndex'
 import pathUtils from 'path'
 import fs from 'src/filesystem/fs'
 
@@ -7,10 +8,18 @@ import types from '../types'
 
 export default async function openFile({ state, rootState, commit }, { path, file }) {
   const location = pathUtils.resolve(rootState.activeProject.path, ...path)
+  const existingTabIndex = findIndex(state.tabs, tab => tab.tabType === 'file' && tab.location === location)
+
+  if (existingTabIndex >= 0) {
+    commit(types.SELECT_TAB, existingTabIndex)
+    return
+  }
+
   const content = await fs.readJsonAsync(location)
   const fileType = find(fileTypes, t => t.id === file.type)
 
   commit(types.ADD_TAB, {
+    location,
     title: file.displayName,
     tabType: 'file',
     fileType: file.type,
