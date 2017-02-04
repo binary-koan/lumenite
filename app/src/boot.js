7bf8cd9c719c28f0d973c4f5ec4516a1a080f803
Jono Mingard <reason.koan@gmail.com>
2017-02-04 22:10:21 +1300

    Add Element UI and PostCSS -> Stylus converter

--- app/src/boot.js
+++ app/src/boot.js
@@ -2,17 +2,19 @@ import './styles/main.styl'
 
 import Vue from 'vue'
 import Electron from 'vue-electron'
+import ElementUI from 'element-ui'
 
 import setupCustomDirectives from './helpers/directives'
 import unsetAnnoyingGlobals from './helpers/unset-annoying-globals'
 
 Vue.use(Electron)
+Vue.use(ElementUI)
 Vue.config.debug = true
 
 setupCustomDirectives(Vue)
 unsetAnnoyingGlobals()
 
-import Main from './main'
+import Main from './element-test'
 
 /* eslint-disable no-new */
 new Vue(Main).$mount('#main')
